069cd8368eebb323a05d997728df5c816c2c548e
scarletsky <scarletsky1025@gmail.com>
2017-12-07 15:51:41 +0800

    chore: make example app can access MasonryLayout source

--- example/config/paths.js
+++ example/config/paths.js
@@ -46,7 +46,7 @@ module.exports = {
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveApp('src/index.js'),
   appPackageJson: resolveApp('package.json'),
-  appSrc: resolveApp('src'),
+  appSrc: [resolveApp('src'), resolveApp('../src')],
   yarnLockFile: resolveApp('yarn.lock'),
   testsSetup: resolveApp('src/setupTests.js'),
   appNodeModules: resolveApp('node_modules'),
