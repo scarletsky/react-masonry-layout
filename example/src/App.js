981d61e108f6711b4cdf06b99fce84ff1a120132
scarletsky <scarletsky1025@gmail.com>
2017-12-07 15:52:17 +0800

    feat: make example app works with MasonryLayout

--- example/src/App.js
+++ example/src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MasonryLayout from 'react-masonry-layout';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    perPage: 10,
+    items: Array(20).fill()
+  }
+
+  loadItems = () => {
+      this.setState({
+        items: this.state.items.concat(Array(this.state.perPage).fill())
+      });
+  }
+
   render() {
     return (
       <div className="App">
+
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Masonry Layout</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <MasonryLayout
+          id="masonry-layout"
+          infiniteScroll={this.loadItems}>
+
+          {this.state.items.map((v, i) => {
+            let height = i % 2 === 0 ? 200 : 100;
+            return (
+              <div
+                key={i}
+                style={{
+                  width: '100px',
+                  height: `${height}px`,
+                  lineHeight: `${height}px`,
+                  color: 'white',
+                  fontSize: '32px',
+                  display: 'block',
+                  background: 'rgba(0,0,0,0.7)'
+                }}>
+                {i}
+              </div>
+            )}
+          )}
+
+        </MasonryLayout>
       </div>
     );
   }
